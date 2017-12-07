8092e07e6a7d4ef9c981e4953a6a809c5d6c9910
llrs0426 <15311257617@163.com>
2017-12-07 22:49:18 +0800

    update

--- RepeatJava/src/com/day12/StringTest3.java
+++ RepeatJava/src/com/day12/StringTest3.java
@@ -1,15 +1,34 @@
 package com.day12;
 /*
  *统计大串中小串出现的次数
-	举例：在字符串” woaijavawozhenaijavawozhendeaijavawozhendehenaijavaxinbuxinwoaijavagun”中
-		  java出现了5次
- 
+ *	举例：在字符串” woaijavawozhenaijavawozhendeaijavawozhendehenaijavaxinbuxinwoaijavagun”中
+ *	  java出现了5次
+ *   
  * */
 public class StringTest3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String s1 = "javawozhenaijavawozhendeaijavawozhendehenaijavaxinbuxinwoaijavagun";
+		String s2 = "java";
+		//思路1：
+//		int ch = -s2.length();
+//		int count = 0;
+//		while(true){
+//			ch = s1.indexOf(s2, ch+s2.length());
+//			if(ch==-1) break;
+//			count++;	
+//		}
+//		System.out.println(s2+"出现的次数为："+count);
+		
+		// 思路2；
+		int ch = 0;
+		int count = 0;
+		while((ch = s1.indexOf(s2))!=-1){
+			count++;
+			s1 = s1.substring(ch+s2.length());
+				
+		}
+		System.out.println(s2+"出现的次数为："+count);
 	}
 
 }
---
+++ RepeatJava/src/com/day12/StringTest4.java
@@ -0,0 +1,43 @@
+package com.day12;
+/*
+ * 寻找两个字符串的最长字串
+ *  输入 abcdefghijklmnop
+		abcsafjklmnopqrstuvw
+	输出
+		jklmnop
+ **/
+public class StringTest4 {
+
+	public static void main(String[] args) {
+		String s1 = "abcdefghijklmnop";
+		String s2 = "abcsafjklmnopqrstuvw";
+
+		String s = findLonString(s1, s2);
+		
+		System.out.println(s);
+	}
+
+	public static String findLonString(String s1, String s2) {
+		String temp = null;
+		//使得s1是两个传中较短的那个，减少比较次数
+		if(s1.length()>s2.length()){
+			temp = s1;
+			s1 = s2;
+			s2 = temp;		
+		}
+		
+		for(int i=s1.length();i>=0;i--){
+			for(int k=0;k<=s1.length()-i;k++){
+				if(s2.contains(s1.substring(k,k+i))){
+					
+					return s1.substring(k,k+i);
+				}
+				
+			}
+			
+		}
+		
+		return null;
+	}
+
+}
